a64e2fbb3ddcb3ff1ec0bb0ee3d5024c771ada51
gulbalasalamov <g.salamov@gmail.com>
2021-08-02 15:42:17 +0300

    Created model class bank account

---
+++ src/model/Account.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.math.BigDecimal;
+
+public class Account {
+    String cardNumber;
+    String pin;
+    BigDecimal balance = new BigDecimal(0);
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
