1bff3cfebb082745b3694dd791bdaacc0ddc15bf
gulbalasalamov <g.salamov@gmail.com>
2021-08-02 18:22:25 +0300

    Created strategy classes and its behaviours

--- src/service/CardAlgorithm.java
+++ src/service/CardAlgorithm.java
@@ -1,5 +1,8 @@
 package service;
 
+/**
+ * The interface that strategy classes implement
+ */
 public interface CardAlgorithm {
     String generateCardNumber();
     String generatePin();
---
+++ src/strategy/CreditCardStrategy.java
@@ -0,0 +1,25 @@
+package strategy;
+
+import service.CardAlgorithm;
+
+import java.util.Random;
+
+public class CreditCardStrategy implements CardAlgorithm {
+    Random random;
+
+    @Override
+    public String generateCardNumber() {
+        long lowerBound = 1_000_000_000L;
+        long upperBound = 9_000_000_000L;
+        long accountNumber = (long) (Math.random() * (upperBound - lowerBound)) + lowerBound;
+        return "400000" + accountNumber;
+    }
+
+    @Override
+    public String generatePin() {
+        int lowerBound = 1000;
+        int upperBound = 9000;
+        int pin = random.nextInt(upperBound - lowerBound) + lowerBound;
+        return String.valueOf(pin);
+    }
+}
