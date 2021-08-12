4c215c56ed842498ff6f43a18913124d344fb4a3
gulbalasalamov <g.salamov@gmail.com>
2021-08-12 17:30:36 +0300

    Main.java : It reads the database file name from the command line argument. Filename should be passed to the program using -fileName argument, for example, -fileName db.s3db.

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,5 @@
 
 D
-src/client/Main.java,b/2/b2b4bcaf937678d09a7cab18c32c5f5bdc8586e6
-D
 src/model/State.java,7/a/7a8d1808ef630b6a12a87764b0a9e7b28633e13c
 Q
 !strategy-pattern-sql-database.iml,4/c/4c5b656bd3834c0835a6b33977d96a7f51ac5099
--- src/client/Main.java
+++ src/client/Main.java
@@ -8,11 +8,25 @@ import java.util.Scanner;
 public class Main {
     /**
      * Prints the main menu and allows user to select from options for further operations
+     * It reads the database file name from the command line argument.
+     * Filename should be passed to the program using -fileName argument
      * @param args
      */
     public static void main(String[] args) {
+
+        String fileName = "";
+        for (int i = 0; i < args.length - 1; i++) {
+            switch (args[i]) {
+                case "-fileName":
+                    fileName = args[i + 1];
+                    break;
+            }
+        }
+
         Scanner scanner = new Scanner(System.in);
         BankingSystem bankingSystem = new BankingSystem();
+        bankingSystem.initializeDatabase(fileName);
+
         while (bankingSystem.state != State.EXIT) {
             System.out.println("1. Create an account\n2. Log into account\n0. Exit");
             String choice = scanner.next();
