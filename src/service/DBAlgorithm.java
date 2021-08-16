e8b455e173040409b9366f867c51217c773b7d94
gulbalasalamov <g.salamov@gmail.com>
2021-08-16 15:19:00 +0300

    DBAlgorithm.java : Interface methods defined for CRUD operations

--- src/service/DBAlgorithm.java
+++ src/service/DBAlgorithm.java
@@ -3,14 +3,22 @@ package service;
 import model.Account;
 import java.sql.Connection;
 
+
 /**
  * The interface that strategy class implement
  */
 public interface DBAlgorithm {
     Connection getConnection(String fileName);
+
     void createTable(Connection connection);
+
     void dropTable(Connection connection);
-    void addAccount(Connection connection,Account account);
-    void loadAccount(Connection connection,Account account);
-    void updateAccount(Connection connection,Account account);
+
+    void addAccount(Connection connection, Account account);
+
+    void updateAccount(Connection connection, String searchCriteria, int amount);
+
+    Account loadAccount(Connection connection, String searchCriteria);
+
+    void deleteAccount(Connection connection, String searchCriteria);
 }
